ce55296228e3a37ed0b662f614de05714f865cfb
Jenssile <jens.e.bouwens@gmail.com>
2024-10-15 14:28:25 +0200

    creat new fex box grid

--- src/TuiLight/lightTui.go
+++ src/TuiLight/lightTui.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"github.com/rivo/tview"
-	"os/exec"
-	"log"
 	"fmt"
+	"github.com/rivo/tview"
+	"log"
+	"os/exec"
 )
 
 func main() {
@@ -13,8 +13,14 @@ func main() {
 		log.Fatal(err)
 	}
 	app := tview.NewApplication()
-	box := tview.NewBox().SetBorder(true).SetTitle(fmt.Sprintf("%s", out))
-	if err := app.SetRoot(box, true).Run(); err != nil {
+	box := tview.NewFlex().
+		AddItem(tview.NewBox().SetBorder(false), 0, 1, false).
+		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+			AddItem(tview.NewBox().SetBorder(false), 0, 1, false).
+			AddItem(tview.NewBox().SetBorder(true).SetTitle(fmt.Sprintf("%s", out)), 0, 3, false).
+			AddItem(tview.NewBox().SetBorder(false), 5, 1, false), 0, 2, false).
+		AddItem(tview.NewBox().SetBorder(false), 0, 1, false)
+	if err := app.SetRoot(box, true).SetFocus(box).Run(); err != nil {
 		panic(err)
 	}
 }
